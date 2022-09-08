ed951c1088ebbcf27b56418d8ab428d09b05a9eb
mahmoud fouad <mfapolice@gmail.com>
2022-09-08 16:40:18 +0200

    done

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 "use strict";
 
+const body = document.querySelectorAll(
+  ".section__feature_boundless-guide-body"
+);
+
+const btns = document.querySelectorAll(
+  ".section__feature_boundless-guide-item"
+);
+
+btns.forEach((el) => {
+  el.addEventListener("click", (e) => {
+    const target = e.currentTarget;
+    const clear = target
+      .querySelector(".section__feature_boundless-guide-body")
+      .classList.contains("active");
+    if (clear) {
+      return target
+        .querySelector(".section__feature_boundless-guide-body")
+        .classList.remove("active");
+    }
+    body.forEach((ele) => ele.classList.remove("active"));
+
+    target
+      .querySelector(".section__feature_boundless-guide-body")
+      .classList.toggle("active");
+  });
+});
+
 console.log("hi there");
